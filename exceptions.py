f9818658843441942aa5bc2fa2d2f043859905f9
Валентин Борыш <valentinborysh@gmail.com>
2022-03-14 21:36:10 +1000

    Исправления после первого ревью

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,14 @@
 class MessageSentError(Exception):
-    ...
+    pass
 
 
 class ResponseError(Exception):
-    ...
+    pass
 
 
 class HomeworksIsNotListError(Exception):
-    ...
+    pass
 
 
 class NoRequiredTokens(Exception):
-    ...
+    pass
--- homework.py
+++ homework.py
@@ -32,25 +32,17 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = StreamHandler(stream=sys.stdout)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 
 def send_message(bot: telegram.Bot, message: str):
     """Функция отправки новых сообшений в Telegram."""
     try:
+        logger.info(f'Сообщение отправлено: {message}')
         bot.send_message(
             chat_id=TELEGRAM_CHAT_ID,
             text=message,
         )
-        logger.info(f'Сообщение отправлено: {message}')
     except MessageSentError:
         logger.error('Ошибка при отправке сообщения')
 
@@ -60,6 +52,7 @@ def get_api_answer(current_timestamp: int):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     try:
+        logger.info('Отправлен запрос к API Яндекс.Домашки')
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         if response.status_code != HTTPStatus.OK:
             error_message = 'Эндпоинт API Яндекс.Домашка недоступен'
@@ -80,7 +73,12 @@ def check_response(response):
         raise HomeworksIsNotListError(
             'Домашние работы пришли не в виде списка'
         )
-    return response.get('homeworks')
+    if (
+            isinstance(response, dict)
+            and 'homeworks' in response
+            and 'current_date' in response
+    ):
+        return response.get('homeworks')
 
 
 def parse_status(homework):
@@ -93,23 +91,21 @@ def parse_status(homework):
             last_homework = homework
         else:
             last_homework = homework[0]
-        homework_name = last_homework.get('homework_name')
-        if not homework_name:
+        if 'homework_name' not in last_homework:
             logger.error(name_error_message)
             raise KeyError(name_error_message)
+        homework_name = last_homework.get('homework_name')
         homework_status = last_homework.get('status')
-        verdict = HOMEWORK_STATUSES.get(homework_status)
-        if not verdict:
+        if homework_status not in HOMEWORK_STATUSES:
             logger.error(status_error_message)
             raise KeyError(status_error_message)
+        verdict = HOMEWORK_STATUSES.get(homework_status)
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверка всех необходимых переменных токенов для работы бота."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        return True
-    return False
+    return all([TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, PRACTICUM_TOKEN])
 
 
 def main():
@@ -136,8 +132,17 @@ def main():
             message = f'Сбой в работе программы: {error}'
             if message != current_status:
                 send_message(bot, message)
-        time.sleep(RETRY_TIME)
+                current_status = message
+        finally:
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    logger.setLevel(logging.DEBUG)
+    handler = StreamHandler(stream=sys.stdout)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     main()
